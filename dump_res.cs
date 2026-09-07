339089156745e8ba8c3d3cb9d387d939eb75a957
YGone <ygone5037@gmail.com>
2026-09-07 14:19:00 +0800

    chore: remove recovery and generated artifacts

--- dump_res.cs
+++
@@ -1,6 +0,0 @@
-using System;
-using System.Reflection;
-class P { static void Main() {
-var asm = Assembly.LoadFile(@"E:\AIHelper-dev\src\bin\Debug\net8.0-windows\AIHelper.dll");
-foreach(var r in asm.GetManifestResourceNames()) Console.WriteLine(r);
-}}
--- tmp_quote_check.cs
+++
@@ -1,5 +0,0 @@
-using AIHelper.Helpers;
-using AIHelper.Services.StockData;
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-var r = await NetworkHelper.GetDataResultAsync("/api/quote?code=000001,600519,300750");
-Console.WriteLine(r.Json);
